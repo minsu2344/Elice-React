f74f30a814093a82e1282a286e4ddba734dc122a
minsoo <minsu2344@naver.com>
2022-06-08 11:36:07 +0900

    버튼 mui 적용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+function Header(){
+  return <header><h1><a href="/">WWW</a></h1></header>
+}
+
+function Article(props) {
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
+function Nav(props) {
+  const liTags = props.data.map((e) => {
+    return <li><a href={'/read/' + e.id}>{e.title}</a></li>
+  })
+  return <nav><ol>{liTags}</ol></nav>
+}
 
 function App() {
+
+  const topics = [
+    {id: 1, title:'html', body: 'html is ...'},
+    {id: 2, title: 'css', body: 'css is ...'},
+  ]
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Nav data={topics}></Nav>
+      <Article title="welcome" body="Hello, WEB!"></Article>
+      <ButtonGroup variant="contained" aria-label="outlined primary button group">
+        <Button variant='outlined'>Create</Button>
+        <Button variant='outlined'>Update</Button>
+      </ButtonGroup>
+      <Button variant='outlined'>Delete</Button>
     </div>
   );
 }
